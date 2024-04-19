1378e5db5c65a7fdcbf7862d385b0b86433b7442
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2024-04-19 15:44:02 +0300

    improved error handling

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/APIAccess.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/APIAccess.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import com.google.gson.JsonParser;
+import static java.lang.Math.abs;
 import java.time.Instant;
 
 public class APIAccess implements iAPI {
@@ -20,14 +21,14 @@ public class APIAccess implements iAPI {
     private boolean has_forecast_info = false;
     private String[][] forecast;
     
-    private static final String API_key = "f52059b774ff5f7508c3b449c6357b9c";
+    private static final String API_KEY = "f52059b774ff5f7508c3b449c6357b9c";
     private static final int FORECAST_DAYS = 7;
     private static final int FORECAST_HOURS = 96;
     private static final int DAILY_FORECAST_INDEX = 0;
     private static final int HOURLY_FORECAST_INDEX = 1;
-    private static final String API_LOCATION_RESPONSE_LIMIT = "1";
-    private static final String test_coord_lon = "23.7609";
-    private static final String test_coord_lat = "61.4981";
+    private static final int API_LOCATION_RESPONSE_LIMIT = 1;
+    private static final int TIME_START_INDEX = 11;
+    private static final int TIME_END_INDEX = 16;
     
     public APIAccess(){
         forecast = new String[2][];
@@ -35,10 +36,10 @@ public class APIAccess implements iAPI {
         forecast[HOURLY_FORECAST_INDEX] = new String [FORECAST_HOURS];
     }
     
-    private void callAPIForCurrent( double latitude, double longitude){
+    private void callAPIForCurrent( double latitude, double longitude) throws Exception{
         String URL_string = String.format(
                 "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s&units=metric"
-                ,latitude, longitude, API_key);
+                ,latitude, longitude, API_KEY);
         try{
             URL url = new URL(URL_string);
             URLConnection connection = url.openConnection();
@@ -55,7 +56,7 @@ public class APIAccess implements iAPI {
             saveCurrentData(data);
         }
         catch(IOException err){
-
+            throw new Exception("API call was unsuccessful");
         }
     }
     
@@ -97,14 +98,14 @@ public class APIAccess implements iAPI {
         has_current_info = true;
     }
     
-    private void callAPIForForecast(double latitude, double longitude){
+    private void callAPIForForecast(double latitude, double longitude) throws Exception{
         
         String URL_stringHour = String.format(
                 "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=%f&lon=%f&appid=%s&units=metric"
-                ,latitude, longitude, API_key);
+                ,latitude, longitude, API_KEY);
         String URL_stringDay = String.format(
                 "https://api.openweathermap.org/data/2.5/forecast/daily?lat=%f&lon=%f&cnt=%d&appid=%s&units=metric"
-                ,latitude, longitude, FORECAST_DAYS, API_key);
+                ,latitude, longitude, FORECAST_DAYS, API_KEY);
         try{
             URL urlHour = new URL(URL_stringHour);
             URL urlDay = new URL(URL_stringDay);
@@ -131,7 +132,7 @@ public class APIAccess implements iAPI {
             saveForecastData(dataDay, dataHour);
         }
         catch(IOException err){
-            
+            throw new Exception("API call was unsuccessful");
         }
     }
     
@@ -193,15 +194,15 @@ public class APIAccess implements iAPI {
     
     private String UTC_UNIXConverter(String input, long timeZone){
         Instant time = Instant.ofEpochSecond(Long.parseLong(input) + timeZone);
-        return time.toString().substring(11,16);
+        return time.toString().substring(TIME_START_INDEX, TIME_END_INDEX);
     }
 
     @Override
-    public String lookUpLocation(String location) {
-        
+    public String lookUpLocation(String location) throws Exception{
+
         String URL_string = String.format(
-                "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=%s&appid=%s"
-                ,location, API_LOCATION_RESPONSE_LIMIT, API_key);
+                "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=%d&appid=%s"
+                ,location, API_LOCATION_RESPONSE_LIMIT, API_KEY);
         try{
             URL url = new URL(URL_string);
             URLConnection connection = url.openConnection();
@@ -215,6 +216,9 @@ public class APIAccess implements iAPI {
             
             JsonParser parse = new JsonParser();
             JsonArray cities = (JsonArray) parse.parse(result);
+            if (cities.isEmpty()){
+                return "";
+            }
             JsonObject cityInfo = cities.get(0).getAsJsonObject();
             
             
@@ -223,13 +227,16 @@ public class APIAccess implements iAPI {
                     cityInfo.get("lon").getAsString());
         }
         catch(IOException err){
-            System.out.println("haistapaska");
+            throw new Exception("API call was unsuccessful");
         }
-        return "";
     }
 
     @Override
-    public String getCurrentWeather(double lat, double lon) {
+    public String getCurrentWeather(double lat, double lon) throws Exception {
+        
+        if (abs(lat) > 90 || abs(lon) > 180){
+            throw new IllegalArgumentException();
+        }  
         if (!has_current_info){
            callAPIForCurrent(lat, lon); 
         }
@@ -237,7 +244,11 @@ public class APIAccess implements iAPI {
     }
 
     @Override
-    public String[][] getForecast(double lat, double lon) {
+    public String[][] getForecast(double lat, double lon) throws Exception {
+        
+        if (abs(lat) > 90 || abs(lon) > 180){
+            throw new IllegalArgumentException();
+        }        
         if (!has_forecast_info){
             callAPIForForecast(lat, lon);
         }
